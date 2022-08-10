ac9057473d1d2b91d1d30a52103a81cb4369f61e
Jayanth <jayanth.koralla.com>
2022-08-10 14:42:10 +0530

    Done create, edit and update with flash and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,36 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title,:description))
+        # render plain: @article.inspect
+        if @article.save
+            flash[:notice] = "Article was created succesfully" 
+            # redirect_to article_path(@article) or
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title,:description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            byebug
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
 end

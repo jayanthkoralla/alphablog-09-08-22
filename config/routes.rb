6dcba0b10011c28072789e4f305302a4454da02c
Jayanth <jayanth.koralla.com>
2022-08-13 11:19:20 +0530

    added users controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
+  get '/signup', to: 'users#new'
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] or 
   resources :articles
+  resources :users, except: [:new]
 end
